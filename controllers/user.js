7762b0c3f6b00ecf88c76cb97f913276b28e0f2f
Christine Wong <jumiryoo@gmail.com>
2023-07-14 18:09:36 -0700

    minor changes to CSS

--- controllers/user.js
+++ controllers/user.js
@@ -42,4 +42,45 @@ router.get('/logout', (req, res) => {
     })
 })
 
+router.get("/newsletter", (req, res) => {
+    res.render('newsletter/signup.ejs');
+})
+
+router.post("/newsletter", (req, res) => {
+    const firstName = req.body.fName;
+    const email = req.body.email
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                name: firstName
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us14.api.mailchimp.com/3.0/lists/aed8dbae06"
+
+    const options = {
+        method: "POST",
+        auth: "chris1:afdd86da2fc852fd331fd12da077cb77-us14"
+    }
+
+   const request = request(url, options, (response) => {
+        if (response.statusCode === 200) {
+            res.render('newsletter/success.ejs');
+        }else {
+            res.render('newsletter/failure.ejs')
+        }
+        res.on("data", (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+})
+
 module.exports = router
